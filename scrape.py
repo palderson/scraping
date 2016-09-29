e7b3d34a2c82c627d561c1b753a297e8c9fdd8f9
Peter Alderson <peter.alderson@gmail.com>
2016-09-29 11:47:29 -0700

    table output

--- scrape.py
+++ scrape.py
@@ -11,18 +11,18 @@ manager = soup.find_all("div", {"class": "member-accordion-list-wrapper"})
 
 for item in manager:
 	try:
-		print item.contents[0].find_all("div", {"class": "member-name"})[0].text
+		print "<tr><td>'%s'</td>" %(item.contents[0].find_all("div", {"class": "member-name"})[0].text)
 	except:
 		pass
 	try:
-		print item.contents[0].find_all("span", {"class": "member-company"})[0].text
+		print "<td>'%s'</td>" %(item.contents[0].find_all("span", {"class": "member-company"})[0].text)
 	except:
 		pass
 	try:		
-		print item.contents[1].find_all("span", {"itemprop": "streetAddress"})[0].text
+		print "<td>'%s'</td>" %(item.contents[1].find_all("span", {"itemprop": "streetAddress"})[0].text)
 	except:
 		pass
 	try:			
-		print item.contents[1].find_all("div", {"class": "phone"})[0].text
+		print "<td>'%s'</td></tr>" %(item.contents[1].find_all("div", {"class": "phone"})[0].text)
 	except:
 		pass
